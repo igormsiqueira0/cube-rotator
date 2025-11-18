d0e1f8d7ddd55c72cc0c813978def31ef0eb45f9
Igor Marinho <igor.siqueira700@gmail.com>
2025-11-18 09:53:18 -0300

    rotação do cubo correta

---
+++ node_3d.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+var tween: Tween
+
+func _process(delta: float) -> void:
+	if tween and tween.is_running():
+		return
+	
+	if Input.is_action_just_pressed("ui_up"):
+		apply_rotation(Vector3.RIGHT, PI/2)
+	elif Input.is_action_just_pressed("ui_down"):
+		apply_rotation(Vector3.RIGHT, -PI/2)
+	elif Input.is_action_just_pressed("ui_left"):
+		apply_rotation(Vector3.UP, PI/2)
+	elif Input.is_action_just_pressed("ui_right"):
+		apply_rotation(Vector3.UP, -PI/2)
+
+func apply_rotation(axis: Vector3, angle: float) -> void:
+	tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	
+	var new_transform = transform.rotated(axis, angle).orthonormalized()
+	
+	tween.tween_property(self, "transform", new_transform, 0.3)
